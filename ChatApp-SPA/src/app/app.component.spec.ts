84c63aa3f306d4e51325cc9b1c11939daa3a105d
luiz Alex <luizalex1997@gmail.com>
2019-10-20 13:15:56 -0300

    Rename DatingApp-SPA to ChatApp-SPA

--- ChatApp-SPA/src/app/app.component.spec.ts
+++ ChatApp-SPA/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DatingApp-SPA'`, () => {
+  it(`should have as title 'ChatApp-SPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DatingApp-SPA');
+    expect(app.title).toEqual('ChatApp-SPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DatingApp-SPA app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ChatApp-SPA app is running!');
   });
 });
